5fe6211c481a62d7a264093592100f1ae15c65f6
Caroline Hansson <carolinehansson@Carolines-MacBook-Air.local>
2021-01-12 22:24:11 +0100

    Hele portfolioen

---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", sidenVises);
+
+
+function sidenVises() {
+    console.log("siden vises");
+    document.querySelector("#solElementSprite").classList.add("pulse");
+    document.querySelector("#omMig.figure").classList.add("slide");
+
+
+}
